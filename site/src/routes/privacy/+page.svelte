a9eb61bb58d933a7b2558b6221f5a9ec649fc9de
thatjar <64643552+thatjar@users.noreply.github.com>
2024-11-10 18:36:41 +0530

    move website into main repo

---
+++ site/src/routes/privacy/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+  <title>1Bot - Privacy Policy</title>
+</svelte:head>
+
+<h1>1Bot Privacy Policy</h1>
+<h2>Privacy is an inalienable human right, and we respect it.</h2>
+<p>
+  As for 1Bot itself, it does not have any Privileged Gateway Intents (which
+  include the Message Content intent) and is technically incapable of reading
+  any messages in any Discord server unless it @mentions 1Bot. It is technically
+  capable of reading direct messages to it, but it does not do so. However, it
+  does collect a minimal amount of data in specific cases - read the next
+  section to know more about it. <br />
+  As for this website, it collects absolutely no data from you.
+</p>
+
+<h2>The only data we collect</h2>
+<p>
+  When you use any of 1Bot's commands, it does not log or store anything unless
+  the command results in an unexpected error. The only data that will be logged
+  in case of an unhandled error is the name of the command, the parameters you
+  provide to it (if any), the time of invocation of the command, as well as your
+  Discord User ID. <br />
+  This is purely to improve the reliability and functionality of the bot, and ensure
+  that the user who invokes the command in question is not a malicious actor who
+  may be attempting to rate-limit the bot from Discord.
+</p>
+<p>
+  For some commands that connect to third parties, the data you include in the
+  command parameters will be sent to the third party. A list of third parties
+  that 1Bot connects to can be found <a href="/third-parties">here</a>.
+</p>
+
+<p>
+  You can see the source code of <a href="https://github.com/thatjar/1bot"
+    >1Bot</a
+  >
+  or <a href="https://github.com/thatjar/1bot-site">this website</a> for yourself.
+  IPs are not collected, zero cookies are used.
+</p>
+
+<div style="height: 50px"></div>
+
+<hr />
+<h4>Last updated: 2024-11-04</h4>

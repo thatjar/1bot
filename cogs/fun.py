f8b7f239a5d6496413fdd6e91183cbdf81b6370b
thatjar <64643552+thatjar@users.noreply.github.com>
2024-11-24 12:53:57 +0530

    add tictactoe command, improve rockpaperscissors

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,12 +11,127 @@ sys.path.insert(0, "/")  # to get access to views module
 from views import Confirm
 
 
+class TicTacToeButton(discord.ui.Button):
+    def __init__(self, x: int, y: int):
+        super().__init__(style=discord.ButtonStyle.secondary, label="\u200b", row=y)
+        self.x = x
+        self.y = y
+
+    async def callback(self, i: discord.Interaction):
+        assert self.view is not None
+        view = self.view
+        state = view.board[self.y][self.x]
+        if state in (view.X, view.O):
+            return
+
+        if view.current_player == view.X and i.user.id == view.p1:
+            self.style = discord.ButtonStyle.danger
+            self.label = "X"
+            self.disabled = True
+            view.board[self.y][self.x] = view.X
+            view.current_player = view.O
+            content = "It is now O's turn"
+        elif view.current_player == view.O and i.user.id == view.p2:
+            self.style = discord.ButtonStyle.success
+            self.label = "O"
+            self.disabled = True
+            view.board[self.y][self.x] = view.O
+            view.current_player = view.X
+            content = "It is now X's turn"
+        elif (
+            view.current_player == view.X
+            and i.user.id == view.p2
+            or view.current_player == view.O
+            and i.user.id == view.p1
+        ):
+            await i.response.send_message("❌ It's not your turn!", ephemeral=True)
+            return
+        else:
+            await i.response.send_message(
+                "❌ You are not part of this game!", ephemeral=True
+            )
+            return
+
+        winner = view.check_board_winner()
+        if winner is not None:
+            if winner == view.X:
+                content = "**X won!**"
+            elif winner == view.O:
+                content = "**O won!**"
+            else:
+                content = "**It's a tie!**"
+
+            for child in view.children:
+                child.disabled = True
+
+            view.stop()
+
+        await i.response.edit_message(content=content, view=view)
+
+
+class TicTacToe(discord.ui.View):
+
+    def __init__(self, p1: discord.User, p2: discord.User):
+        super().__init__(timeout=60)
+        self.X = -1
+        self.O = 1
+        self.Tie = 2
+        self.p1 = p1.id
+        self.p2 = p2.id
+        self.current_player = self.X
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+
+        # Our board is made up of 3 by 3 TicTacToeButtons
+        for x in range(3):
+            for y in range(3):
+                self.add_item(TicTacToeButton(x, y))
+
+    # This method checks for the board winner -- it is used by the TicTacToeButton
+    def check_board_winner(self):
+        for across in self.board:
+            value = sum(across)
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        # Check vertical
+        for line in range(3):
+            value = self.board[0][line] + self.board[1][line] + self.board[2][line]
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        # Check diagonals
+        diag = self.board[0][2] + self.board[1][1] + self.board[2][0]
+        if diag == 3:
+            return self.O
+        elif diag == -3:
+            return self.X
+
+        diag = self.board[0][0] + self.board[1][1] + self.board[2][2]
+        if diag == 3:
+            return self.O
+        elif diag == -3:
+            return self.X
+
+        # If we're here, we need to check if a tie was made
+        if all(i != 0 for row in self.board for i in row):
+            return self.Tie
+
+        return None
+
+
 class RPSButton(discord.ui.Button):
     def __init__(self, label: str, emoji: str):
         super().__init__(label=label, emoji=emoji)
-        self.callback = self.rps
 
-    async def rps(self, i: discord.Interaction):
+    async def callback(self, i: discord.Interaction):
         view = self.view
         if i.user.id not in (view.p1.id, view.p2.id):
             await i.response.send_message(
@@ -71,6 +186,45 @@ class Fun(commands.Cog):
             app_commands.ContextMenu(name="Mock", callback=self.mock_ctx),
         )
 
+    @app_commands.command(name="tictactoe", description="Play Tic Tac Toe")
+    @app_commands.describe(user="The user to play with")
+    @app_commands.checks.cooldown(2, 30, key=lambda i: i.user)
+    async def tictactoe(self, i: discord.Interaction, user: discord.User):
+        if i.user.id == user.id:
+            return await i.response.send_message(
+                "❌ You can't play with yourself!", ephemeral=True
+            )
+            return
+        if user.bot:
+            return await i.response.send_message(
+                "❌ You can't play with a bot!", ephemeral=True
+            )
+
+        view = Confirm(user)
+        await i.response.send_message(
+            content=f"{user.mention}, you have been challenged to **Rock Paper Scissors** by {i.user.mention}! Respond within 60 seconds.",
+            view=view,
+        )
+        await view.wait()
+        if view.accepted is False:
+            return await i.edit_original_response(
+                content=f"The challenge was **rejected** by {user.mention}.", view=None
+            )
+        elif view.accepted is None:
+            return await i.edit_original_response(
+                content=f"{user.mention} did not respond in time.", view=None
+            )
+
+        view = TicTacToe(i.user, user)
+        await i.edit_original_response(
+            content=f"{i.user.mention} as **X** vs. {user.mention} as **O**", view=view
+        )
+        timed_out = await view.wait()
+        if timed_out:
+            await i.edit_original_response(
+                content=":information_source: The game timed out.", view=None
+            )
+
     @app_commands.command(
         name="rockpaperscissors",
         description="Play Rock Paper Scissors with another user",
@@ -90,57 +244,56 @@ class Fun(commands.Cog):
             return
 
         view = Confirm(user)
-        await i.response.defer()
-        first_msg = await i.followup.send(
+        await i.response.send_message(
             f"{user.mention}, you have been challenged to **Rock Paper Scissors** by {i.user.mention}! Respond within 60 seconds.",
             view=view,
         )
         await view.wait()
-        if view.accepted is True:
-            view = RockPaperScissors(i.user, user)
-            embed = discord.Embed(
-                title="Rock Paper Scissors",
-                description=f"### {i.user.mention} vs {user.mention}\nWaiting for players to choose...",
-                colour=self.bot.colour,
-            )
-            await first_msg.edit(content=None, embed=embed, view=view)
-            timed_out = await view.wait()
-            if timed_out:
-                await first_msg.edit(
-                    content="❌ The game timed out.", view=None, embed=None
-                )
-                return
-            if view.winner is not None:
-                winning_choice = (
-                    view.choices[i.user.id]
-                    if view.winner == view.p1
-                    else view.choices[user.id]
-                )
-                losing_choice = (
-                    view.choices[user.id]
-                    if view.winner == view.p1
-                    else view.choices[i.user.id]
-                )
-                embed.description = f"### {view.winner.mention} is the **winner!**"
-                embed.add_field(name="Winning pick:", value=winning_choice)
-                embed.add_field(name="Losing pick:", value=losing_choice)
-                await first_msg.edit(embed=embed, view=None)
-            else:
-                embed.description = "### It's a tie!"
-                embed.add_field(
-                    name="Both players chose:", value=view.choices[i.user.id]
-                )
-                await first_msg.edit(embed=embed, view=None)
-        elif view.accepted is False:
-            await first_msg.edit(
+        if view.accepted is False:
+            return await i.edit_original_response(
                 content=f"The challenge was **rejected** by {user.mention}.",
                 view=None,
             )
         elif view.accepted is None:
-            await first_msg.edit(
+            return await i.edit_original_response(
                 content=f"{user.mention} did not respond in time.",
                 view=None,
             )
+
+        view = RockPaperScissors(i.user, user)
+        embed = discord.Embed(
+            title="Rock Paper Scissors",
+            description=f"### {i.user.mention} vs {user.mention}\nWaiting for players to choose...",
+            colour=self.bot.colour,
+        )
+        await i.edit_original_response(content=None, embed=embed, view=view)
+        timed_out = await view.wait()
+        if timed_out:
+            await i.edit_original_response(
+                content=":information_source: The game timed out.",
+                view=None,
+                embed=None,
+            )
+            return
+        if view.winner is not None:
+            winning_choice = (
+                view.choices[i.user.id]
+                if view.winner == view.p1
+                else view.choices[user.id]
+            )
+            losing_choice = (
+                view.choices[user.id]
+                if view.winner == view.p1
+                else view.choices[i.user.id]
+            )
+            embed.description = f"### {view.winner.mention} is the **winner!**"
+            embed.add_field(name="Winning pick:", value=winning_choice)
+            embed.add_field(name="Losing pick:", value=losing_choice)
+            await i.edit_original_response(embed=embed, view=None)
+        else:
+            embed.description = "### It's a tie!"
+            embed.add_field(name="Both players chose:", value=view.choices[i.user.id])
+            await i.edit_original_response(embed=embed, view=None)
 
     @app_commands.checks.cooldown(1, 20, key=lambda i: i.channel)
     async def quote_ctx(self, i: discord.Interaction, message: discord.Message):

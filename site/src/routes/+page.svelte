676b285e7ef9ee87028820b994494ad337628d3f
thatjar <64643552+thatjar@users.noreply.github.com>
2025-05-29 15:48:04 +0530

    site css changes

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -2,10 +2,7 @@
   <title>1Bot - One bot, several uses.</title>
 </svelte:head>
 
-
 <div class="intro">
-  <br>
-  <br>
   <h1>
     <span class="grey">Meet</span> 1Bot.
   </h1>
@@ -16,11 +13,9 @@
   </p>
   <a
     href="https://discord.com/oauth2/authorize?client_id=884080176416309288"
-    id="add-1bot">Add 1Bot</a>
-    <br>
-    <br>
+    id="add-1bot">Add 1Bot</a
+  >
 </div>
-
 
 <div class="header">
   <h1 style="text-align:center"><span class="grey">Why</span> Choose Us?</h1>
@@ -66,8 +61,7 @@
       <h2>Use Anywhere</h2>
       <p>
         1Bot now supports user installation - add it to your account and use it
-        anywhere you want, including in DMs and group chats. Moderator commands
-        will not be usable via user install.
+        anywhere you want, including in DMs and group chats.
       </p>
     </div>
   </section>
@@ -80,10 +74,24 @@
     flex-direction: column;
     align-items: center;
     text-align: center;
+    background-image: url("/1botbg.png");
+    background-repeat: space;
+    background-size: auto;
+    background-attachment: fixed;
+    padding: 40px 0;
+    width: 100vw;
+    margin-left: calc(-50vw + 50%);
+    margin-top: -30px;
   }
 
   .intro h1 {
     margin: 15px auto;
+  }
+
+  .intro p {
+    color: #ffffff;
+    width: 80%;
+    margin: 15px;
   }
 
   .intro i {
@@ -92,10 +100,6 @@
 
   .grey {
     color: #bebebe;
-  }
-
-  .header {
-    transform: translate(5.5px);
   }
 
   #add-1bot {
@@ -120,7 +124,7 @@
   #add-1bot:active {
     transform: translateY(2px);
     box-shadow: none;
-    color:rgb(217, 125, 45)
+    color: rgb(217, 125, 45);
   }
 
   .features {
@@ -152,24 +156,22 @@
     transform: translateY(-4px);
     transition: 300ms;
     font-size: 150%;
-    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     color: rgb(217, 125, 45);
   }
 
-  .card p,a {
+  .card p {
     transition: 200ms;
     color: #ffffff;
     font-size: 120%;
   }
 
   .card:hover p {
-    color: rgb(255,255,255);
+    color: rgb(255, 255, 255);
     font-size: 120%;
   }
 
   .card:hover a {
     color: #fd9d03e6;
-    font-size: 120%;
   }
 
   .card {

bfbe7479f4495e499a54a9184b18e4c70b20d64e
thatjar <64643552+thatjar@users.noreply.github.com>
2025-07-05 01:58:40 +0530

    fix

--- utils/utils.py
+++ utils/utils.py
@@ -9,37 +9,43 @@ class Embed(discord.Embed):
     """Embed with automatic truncating."""
 
     def __init__(self, *args, **kwargs):
-        # Truncate the description to 4096 characters
         if kwargs.get("description"):
             description = kwargs["description"]
-            if len(description) > 4096:
-                kwargs["description"] = description[:4093] + "..."
-        # Truncate the title to 256 characters
+            if type(description) is str:
+                description = description.strip()
+                if len(description) > 4096:
+                    kwargs["description"] = description[:4093] + "..."
         if kwargs.get("title"):
             title = kwargs["title"]
-            if len(title) > 256:
-                kwargs["title"] = title[:253] + "..."
+            if type(title) is str:
+                title = title.strip()
+                if len(title) > 256:
+                    kwargs["title"] = title[:253] + "..."
         super().__init__(*args, **kwargs)
 
     def add_field(self, *, name, value, inline=True):
-        # Truncate the field name to 256 characters
-        if len(name) > 256:
-            name = name[:253] + "..."
-        # Truncate the field value to 1024 characters
-        if len(value) > 1024:
-            value = value[:1021] + "..."
+        if type(name) is str:
+            name = name.strip()
+            if len(name) > 256:
+                name = name[:253] + "..."
+        if type(value) is str:
+            value = value.strip()
+            if len(value) > 1024:
+                value = value[:1021] + "..."
         return super().add_field(name=name, value=value, inline=inline)
 
     def set_footer(self, *, text=None, icon_url=None):
-        # Truncate the footer text to 2048 characters
-        if text and len(text) > 2048:
-            text = text[:2045] + "..."
+        if type(text) is str:
+            text = text.strip()
+            if len(text) > 2048:
+                text = text[:2045] + "..."
         return super().set_footer(text=text, icon_url=icon_url)
 
     def set_author(self, *, name=None, icon_url=None, url=None):
-        # Truncate the author name to 256 characters
-        if name and len(name) > 256:
-            name = name[:253] + "..."
+        if type(name) is str:
+            name = name.strip()
+            if len(name) > 256:
+                name = name[:253] + "..."
         return super().set_author(name=name, icon_url=icon_url, url=url)
 
 
